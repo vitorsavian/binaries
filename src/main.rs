ea067398685f1b1779eb968c5c92ad9691867701
Vitor Savian <vitor.savian@suse.com>
2023-11-19 03:54:01 -0300

    Testing getting entries

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::fs;
+use std::env;
+use std::io;
+use std::path::PathBuf;
+
 fn main() {
     println!("Hello, world!");
+    match env::current_dir() {
+        Ok(path) => {
+            let entries = ls(path);        
+        }
+        Err(e) => {
+            println!("Unable to get path: {}", e);
+        }
+    }
+}
+
+fn ls(path: PathBuf) -> io::Result<()> {
+    let mut entries = fs::read_dir(path)?
+    .map(|res| res.map(|entry| entry.file_name()))
+    .collect::<Result<Vec<_>, io::Error>>()?;
+
+    entries.sort();
+
+    Ok(())
 }
