23ea261fb9a8a90d1ab6905dad56b2112e67cf2f
Vitor Savian <vitor.savian@suse.com>
2023-12-17 17:57:25 -0300

    Refactor to make ls a run in cmd, but need more refactoring

--- src/bin/ls/main.rs
+++ src/bin/ls/main.rs
@@ -1,8 +1,6 @@
-// use std::{env, os::unix::fs::PermissionsExt};
-
+use binaries::cmd::ls::run;
 fn main() {
-    
-
+    run();
     // let args = env::args().collect::<Vec<String>>();
 
     // args.iter().for_each(|arg| {
@@ -38,27 +36,4 @@ fn main() {
     // } else {
     //     println!("Error");
     // }
-
-    // let path = env::current_dir().unwrap();
-
-    // path.read_dir().unwrap()
-    //     .for_each(|entry| {
-    //         let dir = entry.unwrap();
-    //         let permissions = dir.metadata().unwrap().permissions();
-    //         let permissions_string = match permissions.mode() & 0o777 {
-    //             0o755 => "rwxr-xr-x",
-    //             0o644 => "rw-r--r--",
-    //             0o600 => "rw-------",
-    //             0o666 => "rw-rw-rw-",
-    //             0o777 => "rwxrwxrwx",
-    //             0o000 => "---------",
-    //             0o111 => "--x--x--x",
-    //             0o222 => "-w--w--w-",
-    //             0o333 => "-wx-wx-wx",
-    //             0o444 => "r--r--r--",
-    //             0o555 => "r-xr-xr-x",
-    //             _ => "---------",
-    //         };
-    //         println!("{}\t{}", permissions_string, dir.file_name().to_str().unwrap());
-    //     });
 }
--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,1 +1,2 @@
 pub mod pwd;
+pub mod ls;
